5e3751376f1ef34b1ee917c36a42054e8dece0e1
ocar1053 <ocar8951@gmail.com>
2025-07-18 15:51:28 +0800

    Add YOLO v12 model deployment and inference handler

---
+++ serverless/pytorch/yolov12/ocarpan/nuclio/main.py
@@ -0,0 +1,45 @@
+from ultralytics import YOLO
+import json
+import base64
+import io
+from PIL import Image
+import cv2
+import numpy as np
+def init_context(context):
+
+    context.logger.info("Init context...  0%")
+    model = YOLO('best.pt')  # load a pretrained model
+    context.user_data.model_handler = model
+
+    context.logger.info("Init context...100%")
+
+def handler(context, event):
+
+	context.logger.info('Run custom yolov12 model')
+	data = event.body
+	image_buffer = io.BytesIO(base64.b64decode(data['image']))
+	image = cv2.imdecode(np.frombuffer(image_buffer.getvalue(), np.uint8), cv2.IMREAD_COLOR)
+
+	results = context.user_data.model_handler(image)
+	result = results[0]
+
+	boxes = result.boxes.data[:,:4]
+	confs = result.boxes.conf
+	clss = result.boxes.cls
+	class_name = result.names
+
+	detections = []
+	threshold = 0.3
+	for box, conf, cls in zip(boxes, confs, clss):
+		label = class_name[int(cls)]
+		if conf >= threshold:
+			# must be in this format
+			detections.append({
+				'confidence': str(float(conf)),
+				'label': label,
+				'points': box.tolist(),
+				'type': 'rectangle',
+			})
+
+	return context.Response(body=json.dumps(detections), headers={},
+		content_type='application/json', status_code=200)
